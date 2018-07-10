bb0558d9e276ba328bb2ed4cb2b670bd00542dd1
Joselyn Moya <joselyn.moyap@gmail.com>
2018-07-10 18:05:57 -0400

    se mejora menu inicio

--- login/panel-control.php
+++ login/panel-control.php
@@ -1,4 +1,5 @@
 <?php
+include_once '../dto/Usuario.php';
 $is_logged_in = isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true;
 if (!$is_logged_in) {
     echo "Esta pagina es solo para usuarios registrados.<br>";
@@ -44,13 +45,21 @@ if ($now > $_SESSION['expire']) {
                                             <br>
                                             <p><a class="btn purple lighten-1" href="../paginas/ventanaBusquedaMuestras.php" >Mis Análisis</a></p>
                                         <?php } else { ?>
+                                            
                                             <p><a class="btn purple lighten-1" href="../paginas/ventanaRecepcionMuestras.php" >Mis Datos</a></p>
                                             <br>
+                                            <?php if($_SESSION['usuario']->getCategoria() == 'R') {?>
                                             <p><a class="btn purple lighten-1" href="../paginas/ventanaBusquedaMuestras.php" >Muestras Recibidas</a></p>
                                             <br>
                                             <p><a class="btn purple lighten-1" href="../paginas/ventanaBuscarCliente.php" >Ingresar Muestras</a></p>
                                             <br>
+                                            <?php }
+                                            if ($_SESSION['usuario']->getCategoria() == 'T') { ?>
                                             <p><a class="btn purple lighten-1" href="../paginas/ventanaListadoMuestras.php" >Muestras en Proceso</a></p>
+                                            <?php } 
+                                            if ($_SESSION['usuario']->getCategoria() == 'A') { ?>
+                                            
+                                            <?php }?>
                                         <?php } ?>
                                     </ul>
                                 </div>
