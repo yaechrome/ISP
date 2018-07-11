d4c7716377eeab0777ac204ee6bfbea5f0cb4895
Yaeko Ogino <yaekoogino@gmail.com>
2018-07-11 19:36:30 -0400

    Se agrega html de mantenedor y crear de TipoAnalisis

---
+++ paginas/ventanaCrearTipoAnalisis.php
@@ -0,0 +1,36 @@
+<?php
+// put your code here
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Crear Tipo de Analisis</title>
+        <style>
+            .container-busquedas {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: space-around;
+            }
+            .grilla {
+                display: grid;
+                grid-template-columns: 50% 50%;
+                grid-gap: 10px;
+            }
+            .header {
+                border-bottom: 1px solid #000;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="agregarTipoAnalisis.php" method="POST" class="container-busquedas">
+            <div class="grilla">
+                <div>Nombre</div>
+                <div><input type="text" name="txtNombre" value="" /></div>
+                <input type="submit" value="Guardar" name="btnGuardar" />
+            </div>  
+        </form>
+        <a href=../login/volver.php>Volver</a> <br>
+    </body>
+</html>
---
+++ paginas/ventanaMantenedorTipoAnalisis.php
@@ -0,0 +1,51 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Mantenedor Tipo Analisis</title>
+        <style>
+            .container-busquedas {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+            }
+            .grilla {
+                display: grid;
+                grid-template-columns: 4fr 4fr 2fr;
+                grid-gap: 10px;
+            }
+            .header {
+                border-bottom: 1px solid #000;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="mantenedorTipoAnalisis.php" method="POST" class="container-busquedas">
+            <div>ID</div>
+            <input type="text" name="txtId" value="" />
+            <input type="submit" value="Buscar" name="btnBuscar" />
+        </form>
+        <br>
+        <div class="grilla">
+            <div class="header">ID</div>
+            <div class="header">Nombre</div>
+            <div class="header"></div>
+            <?php
+                    for ($x = 0; $x <= 10; $x++) {
+                        ?>
+                            <div>123</div>
+                            <div>456</div>
+                            <div><input type="button" value="Eliminar" name="btnEliminar" /></div>
+                        <?php
+                    }
+                ?>
+        </div>
+        <a href="ventanaCrearTipoAnalisis.php">Crear</a>
+        <a href=../login/volver.php>Volver</a> <br>
+    </body>
+</html>
