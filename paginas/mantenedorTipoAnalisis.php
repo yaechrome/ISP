4fc40a732c4be9b65c164c9cfaa8d18de1c20647
Joselyn Moya <joselyn.moyap@gmail.com>
2018-07-11 22:05:22 -0400

    correccion listar todos de mantenedorTipoAnalisis

--- paginas/mantenedorTipoAnalisis.php
+++ paginas/mantenedorTipoAnalisis.php
@@ -1,25 +1,28 @@
 <?php
+
 include_once '../dao/TipoAnalisisDaoImp.php';
 include_once '../login/sessionStart.php';
 
-
-$id = trim($_POST["txtId"]);
 $dao = new TipoAnalisisDaoImp();
-$dto = $dao->buscarPorClavePrimaria($id);
-$lista = array($dto);
 
-if(!$dto->getId() == $id){
-    $mensaje = 'No existe tipo de analisis con ese ID';
-    $lista = $dao->listar();
-    $_SESSION["listaDesplegar"]= $lista;
-} else{
-    $_SESSION["listaDesplegar"]= $lista;
+$mensaje = null;
+if (isset($_POST["txtId"])) {
+    $id = $_POST["txtId"];
+    $dto = $dao->buscarPorClavePrimaria($id);
+    $lista = array($dto);
+    if (!$dto->getId() == $id) {
+        $mensaje = 'No existe tipo de analisis con ese ID';
+        $lista = $dao->listar();
+        $_SESSION["listaDesplegar"] = $lista;
+    } else {
+        $_SESSION["listaDesplegar"] = $lista;
+    }
 }
 
 
 
-if($mensaje!= null){
-        echo "<script> alert('$mensaje') </script>";
+if ($mensaje != null) {
+    echo "<script> alert('$mensaje') </script>";
 }
 
 include_once 'ventanaMantenedorTipoAnalisis.php';
