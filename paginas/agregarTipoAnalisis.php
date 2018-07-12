23d6b8342a68db0982d906a236417a5971ce2fbe
Yaeko Ogino <yaekoogino@gmail.com>
2018-07-12 05:29:35 -0400

    Se agrega listaDespegar para que se actualice al crear un tipo analisis la lista

--- paginas/agregarTipoAnalisis.php
+++ paginas/agregarTipoAnalisis.php
@@ -1,16 +1,19 @@
 <?php
+
 include_once '../dao/TipoAnalisisDaoImp.php';
 include_once '../login/sessionStart.php';
 
 $nombre = $_POST["txtNombre"];
 $dao = new TipoAnalisisDaoImp();
 $mensaje = null;
-if($dao->existeRegistro($nombre)){
+if ($dao->existeRegistro($nombre)) {
     $mensaje = 'Tipo de analisis ya existe';
-}else{
-    if($dao->crear($nombre)){
+} else {
+    if ($dao->crear($nombre)) {
         $mensaje = 'Tipo de analisis creado correctamente';
-    }else{
+        $lista = $dao->listar();
+        $_SESSION["listaDesplegar"] = $lista;
+    } else {
         $mensaje = 'No se pudo crear Tipo de analisis';
     }
 }
