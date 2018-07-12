fb8451ef828584bc4c5a323036f487a68c64f5a7
Yaeko Ogino <yaekoogino@gmail.com>
2018-07-12 04:13:51 -0400

    Validacion

--- paginas/ventanaListadoMuestras.php
+++ paginas/ventanaListadoMuestras.php
@@ -3,11 +3,9 @@ include_once '../dao/AnalisisMuestraDaoImp.php';
 include_once '../login/sessionStart.php';
 
 
-    $dao = new AnalisisMuestraDaoImp();
-    $lista = $dao->buscarEnProceso();
-    $_SESSION["listaDesplegar"] = $lista;
-
-
+$dao = new AnalisisMuestraDaoImp();
+$lista = $dao->buscarEnProceso();
+$_SESSION["listaDesplegar"] = $lista;
 ?>
 
 <!DOCTYPE html>
@@ -21,23 +19,33 @@ include_once '../login/sessionStart.php';
                 grid-template-columns: 34% 34% 32%;
                 grid-gap: 10px;
             }
+            .header {
+                border-bottom: 1px solid #000;
+            }
         </style>
     </head>
     <body>
         <h1>Listado de muestras para procesar</h1>
         <form action="listadoMuestras.php" method="POST">
             <div class="grid-wrapper">
-                <div>Código Usuario</div>
-                <div>Código Muestra</div>
-                <div>Acción</div>
+                <div class="header">Código Usuario</div>
+                <div class="header">Código Muestra</div>
+                <div class="header">Acción</div>
                 <?php
                 include_once '../dto/AnalisisMuestras.php';
-            foreach ($lista as $dto) {
-                ?>
-                <div><?php echo $dto->getUsuario()->getCodigo(); ?></div>
-                <div><?php echo $dto->getId(); ?></div>
-                <a href="ventanaRegistroMuestras.php?id=<?= $dto->getId(); ?>&codigo=<?= $dto->getUsuario()->getCodigo(); ?>">Procesar</a>
-            <?php } ?>
+                $data = $_SESSION["listaDesplegar"];
+                if (count($data) > 0) {
+                    foreach ($data as $dto) {
+                        ?>
+                        <div><?php echo $dto->getUsuario()->getCodigo(); ?></div>
+                        <div><?php echo $dto->getId(); ?></div>
+                        <a href="ventanaRegistroMuestras.php?id=<?= $dto->getId(); ?>&codigo=<?= $dto->getUsuario()->getCodigo(); ?>">Procesar</a>
+                        <?php
+                    }
+                } else {
+                    ?>
+                    <div><?php echo 'Sin muestras para procesar' ?></div>
+                <?php } ?>
             </div>
 
         </form>
