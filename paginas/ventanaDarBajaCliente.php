58160efa83ef8206f5edeaaaafc95c73c4de8525
ochandia <os.chandia@alumnos.duoc.cl>
2018-07-12 06:47:29 -0300

    actualizacion de esta vista

--- paginas/ventanaDarBajaCliente.php
+++ paginas/ventanaDarBajaCliente.php
@@ -8,8 +8,7 @@ function post($key) {
     return isset($_POST[$key]) ? $_POST[$key] : "";
 }
 
-function htmlAlert($msg)
-{
+function htmlAlert($msg) {
     echo '<script type="text/javascript">alert("' . $msg . '")</script>';
 }
 
@@ -17,35 +16,31 @@ if (post('accion') == 'Dar de baja') {
     $dao = new UsuarioDaoImp();
     $codigo = post('codigo');
     $sePudoDarDeBaja = $dao->darDeBaja($codigo);
-    
-    $mensaje = $sePudoDarDeBaja
-        ? "Se dio de baja al cliente número $codigo."
-        : "Hubo un problema al intentar dar de baja al usuario número $codigo.";
-    
+
+    $mensaje = $sePudoDarDeBaja ? "Se dio de baja al cliente número $codigo." : "Hubo un problema al intentar dar de baja al usuario número $codigo.";
+
     htmlAlert($mensaje);
 }
 
-function personas()
-{
+function personas() {
     $dao = new UsuarioDaoImp();
-    
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $filtro = post('filtro');
         $metodo_busqueda = post('Buscar');
-    
+
         switch ($metodo_busqueda) {
             case 'RUT':
                 $persona = $dao->buscarPorRutCliente($filtro);
                 return $persona ? array($persona) : array();
-        
+
             case 'Perfil':
                 return iterator_to_array($dao->buscarPorPerfil($filtro));
         }
     }
-    
+
     return iterator_to_array($dao->listar());
 }
-
 
 $htmlDeUnaPersona = function ($persona) {
     $htmlDeUnDato = function ($dato) {
@@ -77,61 +72,68 @@ $htmlDeTodasLasPersonas = implode('', $htmlsDeTodasLasPersonas);
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="../static/css/postulacion.css" type="text/css"/>
         <meta charset="UTF-8">
         <title>Dar baja a Cliente</title>
         <style>
-            .container-busquedas {
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: space-around;
-            }
-            .container-busquedas2 {
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: center;
-            }
-            .busqueda {
-                width: 50%;
-                min-width: 300px;
-            }
             .grilla {
                 display: grid;
                 grid-template-columns: 3fr 3fr 3fr 1fr;
                 grid-gap: 10px;
             }
-            .header {
-                border-bottom: 1px solid #000;
-            }
+
         </style>
     </head>
     <body>
-        <h1>Dar de baja a Clientes</h1>
-        <div class="container-busquedas">
-            <form method="POST" class="container-formulario">
-                <div>Rut</div>
-                <input type="text" name="filtro" value="" placeholder="12.345.678-9">
-                <input type="submit" name="Buscar" value="RUT">
-            </form>
-            <form method="POST" class="container-formulario">
-                <div>Perfil</div>
-                <select name="filtro">
-                    <?php selectPerfilesClientes('Particular'); ?>
-                </select>
-                <input type="submit" name="Buscar" value="Perfil">
-            </form>
-        </div>
-        <div class="container-busquedas2">
-            <form method="POST">
-                <input type="submit" value="Listar Todos" name="btnListarTodos" />
-            </form>
-        </div>
-        <div class="grilla">
-            <div class="header">Rut</div>
-            <div class="header">Nombre</div>
-            <div class="header">Perfil</div>
-            <div class="header"></div>
-            <?= $htmlDeTodasLasPersonas ?>
-        </div>
-        <a href=../login/volver.php>Volver</a> <br>
+        <main role="main">
+            <section class="container">
+                <div class="row mb0 center-align relative full">
+                    <div class="center">
+                        <div class="col s12 m6 offset-m3 l4 offset-l4">
+                            <div class="card">
+                                <div class="card-panel pad0">
+                                    <div class="card-content pad24">
+                                        <div class="mb20"><h3 class="medium title">Dar de baja a Clientes</h3></div>  
+
+                                        <div class="container-busquedas">
+                                            <form method="POST" class="container-formulario">
+                                                <div>Rut</div>
+                                                <input type="text" name="filtro" value="" placeholder="12.345.678-9">
+                                                <input type="submit" name="Buscar" value="RUT">
+                                            </form>
+                                            <br>
+                                            <form method="POST" class="container-formulario">
+                                                <div>Perfil</div>
+                                                <select name="filtro">
+                                                    <?php selectPerfilesClientes('Particular'); ?>
+                                                </select>
+                                                <input type="submit" name="Buscar" value="Perfil">
+                                            </form>
+                                        </div>
+                                        <br>
+                                        <div class="container-busquedas2">
+                                            <form method="POST">
+                                                <input type="submit" value="Listar Todos" name="btnListarTodos" />
+                                            </form>
+                                        </div>
+                                        <br>
+                                        <div class="grilla">
+                                            <div class="header">Rut</div>
+                                            <div class="header">Nombre</div>
+                                            <div class="header">Perfil</div>
+                                            <div class="header">Acción</div>
+                                            <?= $htmlDeTodasLasPersonas ?>
+                                        </div>
+                                        <br>
+                                        <a href=../login/volver.php>Volver</a> <br>
+
+                                    </div>
+                                </div>
+                            </div>   
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     </body>
 </html>
